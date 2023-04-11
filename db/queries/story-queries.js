9f5e066077b79e5debabecb92466544a15c7b078
stanw0ng <user.stanley@gmail.com>
2023-04-11 15:43:38 -0400

    added database data to read template

--- db/queries/story-queries.js
+++ db/queries/story-queries.js
@@ -1,5 +1,6 @@
 const db = require('../connection');
 
+// fetches all stories
 const getStories = (limit = 10) => {
   return db.query(`SELECT * FROM stories ORDER BY date_created DESC LIMIT $1;`, [limit])
     .then(user => {
@@ -10,6 +11,7 @@ const getStories = (limit = 10) => {
     });
 };
 
+// fetches all stories created by current user
 const getMyStories = (userId) => {
   return db.query('SELECT * FROM stories JOIN users ON author_id = users.id WHERE email = $1', [userId])
     .then(res => {
@@ -19,6 +21,38 @@ const getMyStories = (userId) => {
       return null;
     });
 };
+
+// fetch root data for a story
+const getRootChapter = (storyId) => {
+  return db.query(`SELECT stories.story_title, chapters.title, chapters.id, chapters.body
+  FROM stories
+  JOIN chapters ON stories.chapter_id = chapters.id
+  WHERE stories.id = $1
+  `, [storyId])
+    .then(res => {
+     return res.rows[0];
+    })
+    .catch(err => {
+      return null;
+    });
+};
+
+// WIP should fetch winning contributions
+const getChildrenChapters = (storyId) => {
+  return db.query(`SELECT winners.child_id, chapters.title
+  FROM winners
+  JOIN stories ON winners.story_id = stories.id
+  JOIN contributions ON winners.child_id = contributions.id
+  JOIN chapters ON contributions.chapter_id = chapters.id
+  WHERE stories.id = $1
+  `, [storyId])
+  .then(user => {
+    return user.rows;
+  })
+  .catch(err => {
+    return null;
+  });
+}
 
 const getChapter = (chapter_id) => {
   return db.query(`SELECT title, text FROM chapters WHERE chapter_id = $1`, [chapter_id])
@@ -42,4 +76,4 @@ const getStoryContributions = (story_id) => {
   return db.query(`SELECT contributions.* FROM contributions WHERE story_id = $1`, [story_id]);
 };
 
-module.exports = { getStories, getMyStories, getChapter, getBookmarkedStories, getUserContributions, getStoryContributions };
+module.exports = { getStories, getMyStories, getRootChapter, getChildrenChapters, getChapter, getBookmarkedStories, getUserContributions, getStoryContributions };
--- routes/read.js
+++ routes/read.js
@@ -19,8 +19,23 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get('/:id', (req, res) => {
-  return res.render('read')
+// renders page for story starting from root chapter and chapters
+router.get("/:id", (req, res) => {
+  const storyId = req.params.id;
+  const getRootChapterPromise = storyQueries.getRootChapter(storyId)
+  const getChildrenChaptersPromise = storyQueries.getChildrenChapters(storyId)
+
+  Promise.all([getRootChapterPromise, getChildrenChaptersPromise])
+    .then(data => {
+      const [rootChapter, childrenChapters] = data;
+      const templateVars = { rootChapter, childrenChapters };
+      return res.render('read', templateVars);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send("Error retrieving stories");
+    });
 });
+
 
 module.exports = router;

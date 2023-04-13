b2aae623e3b995bfe314336652958e680cd5d683
KovaKreative <eric.kovalevskyy@gmail.com>
2023-04-13 12:14:15 -0400

    Implemented closing the story

--- db/queries/manage-queries.js
+++ db/queries/manage-queries.js
@@ -1,7 +1,6 @@
 const db = require('../connection');
 
 const setPublishedChapter = (chapterID, publish) => {
-  console.log(publish);
   return db.query(`UPDATE chapters SET published = $1 WHERE id = $2 RETURNING published`, [publish, chapterID])
     .then(published => {
       return published.rows[0];
@@ -13,7 +12,14 @@ const setPublishedChapter = (chapterID, publish) => {
 };
 
 const setCloseStory = (storyID, close) => {
-
+  return db.query(`UPDATE stories SET completed = $1 WHERE id = $2 RETURNING completed`, [close, storyID])
+    .then(closed => {
+      return closed.rows[0];
+    })
+    .catch(err => {
+      console.log(err);
+      return false;
+    })
 };
 
 module.exports = { setPublishedChapter, setCloseStory };
--- routes/my-story.js
+++ routes/my-story.js
@@ -26,19 +26,21 @@ router.post('/publish/:chapter/:publish', (req, res) => {
   manageQueries.setPublishedChapter(chapterID, publish)
     .then((result) => {
       if (!result) {
-        res.render('/error', { message: "Something went wrong. Could not change published status." });
+        return res.render('/error', { message: "Something went wrong. Could not change published status." });
       }
-      console.log(result);
       res.redirect('/read');
     });
-});
+  });
 
-router.post('/close/:id/:close', (req, res) => {
-  const storyID = req.params.id;
-  const close = req.params.close;
+  router.post('/close/:id/:close', (req, res) => {
+    const storyID = req.params.id;
+    const close = req.params.close;
 
-  manageQueries.setCloseStory(storyID, close)
-    .then(() => {
+    manageQueries.setCloseStory(storyID, close)
+    .then((result) => {
+      if(!result) {
+        return res.redirect('/read');
+      }
       res.redirect('/read');
     });
 });
